237c4e9bcef028d9c4a0f303e3c4f18e669255eb
RuyBarbosa22 <ruy.castro@sptech.school>
2022-11-18 18:17:03 -0300

    Adicionando novas tabelas da API de Kotlin

--- src/database/scriptBanco.sql
+++ src/database/scriptBanco.sql
@@ -5,7 +5,9 @@ create table empresa (
     cnpj char (18) not null,
     cep char (8) not null,
     estado varchar (45) not null,
-    numero char not null
+    numero varchar (13) not null,
+    senha varchar (25) not null,
+    codEmpresa varchar (25) not null
 );
 
 create table Usuario (
@@ -14,6 +16,7 @@ create table Usuario (
     email varchar (30) not null unique,
     tel varchar (11) not null unique,
     senha varchar (20) not null,
+    codEmpresa varchar (25) not null,
     fk_empresa int not null
 );
 
@@ -26,8 +29,20 @@ create table computador (
     cpu_nucleos_fisicos int not null,
 	cpu_freq_maxima float not null,
     memoria_total float not null,
+    codEmpresa varchar (25) not null,
     fk_empresa int not null
 );
+
+create table computador_kotlin (
+    id int primary key identity (200,1),
+    foreign key (fk_empresa) references empresa(id),
+    sistema_operacional varchar (45) not null,
+    disco_total float not null,
+	cpu_nucleos_logicos int not null,
+    cpu_nucleos_fisicos int not null,
+    memoria_total float not null,
+    fk_empresa int not null
+)
 
 create table disco_dinamico (
 	id int primary key identity (300,1),
@@ -40,7 +55,25 @@ create table disco_dinamico (
     fk_computador int not null
 );
 
+create table disco_dinamico_kotlin (
+	id int primary key identity (300,1),
+    foreign key (fk_computador) references computador(id),
+    total float,
+    qtdDisco int,
+    dataHora datetime,
+    fk_computador int not null
+);
+
 create table cpu_dinamica (
+	id int primary key identity (500,1)
+    foreign key (fk_computador) references computador(id),
+	pct_uso float,
+    freq_uso float,
+    dataHora datetime,
+    fk_computador int not null
+);
+
+create table cpu_dinamica_kotlin (
 	id int primary key identity (500,1)
     foreign key (fk_computador) references computador(id),
 	pct_uso float,
@@ -55,6 +88,16 @@ create table memoria_dinamica (
     mem_total float,
     mem_usando float,
     mem_usando_pct float,
+    mem_livre float,
+    dataHora datetime,
+    fk_computador int not null
+);  
+
+create table memoria_dinamica_kotlin (
+    id int primary key identity (200,2),
+    foreign key (fk_computador) references computador(id), 
+    mem_total float,
+    mem_usando float,
     mem_livre float,
     dataHora datetime,
     fk_computador int not null
